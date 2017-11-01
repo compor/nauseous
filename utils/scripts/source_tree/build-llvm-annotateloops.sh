9041953d4b267f7f19e298cea8251cd6ad08f71d
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-11-01 12:21:25 +0000

    add pipeline for loop annotation

--- utils/scripts/source_tree/build-llvm-annotateloops.sh
+++ utils/scripts/source_tree/build-llvm-annotateloops.sh
@@ -8,41 +8,21 @@ INSTALL_PREFIX=""
 
 # set configuration vars
 
-if [ -z "$1" ]; then 
-  echo "error: source directory was not provided" 
-
-  exit 1
-fi
-
+[[ -z $1 ]] && echo "error: source directory was not provided" && exit 1
 SRC_DIR=$1
 
-if [ -z "$2" ]; then 
-  INSTALL_PREFIX="${SRC_DIR}/../install/"
-else
-  INSTALL_PREFIX="$2"
-fi
+INSTALL_PREFIX="$2"
+[[ -z $2 ]] && INSTALL_PREFIX="${SRC_DIR}/../install/"
 
+[[ -z $AnnotateLoops_DIR ]] && echo "error: AnnotateLoops_DIR is not set" && exit 2
 
-PIPELINE_CONFIG_FILE="${SRC_DIR}/config/pipelines/pollyplain.txt"
+PIPELINE_CONFIG_FILE="${SRC_DIR}/config/pipelines/annotateloops.txt"
 BMK_CONFIG_FILE="${SRC_DIR}/config/suite_all.txt"
 BMK_CLASS="S"
 
-if [ -z ${LLVMPOLLY_ROOT+x} ]; then 
-  echo "error: LLVMPOLLY_ROOT is not set"
+#
 
-  exit 2
-fi
-
-
-# print configuration vars
-
-echo "info: printing configuration vars"
-echo "info: source dir: ${SRC_DIR}"
-echo "info: install dir: ${INSTALL_PREFIX}"
-echo ""
-
-
-C_FLAGS="-g -Wall -O3"
+C_FLAGS="-g -Wall"
 LINKER_FLAGS="-Wl,-L$(llvm-config --libdir) -Wl,-rpath=$(llvm-config --libdir)"
 LINKER_FLAGS="${LINKER_FLAGS} -lc++ -lc++abi" 
 
@@ -53,6 +33,7 @@ CC=clang CXX=clang++ \
   -DLLVM_DIR=$(llvm-config --prefix)/share/llvm/cmake/ \
   -DCMAKE_BUILD_TYPE=Debug \
   -DCMAKE_C_FLAGS="${C_FLAGS}" \
+  -DCMAKE_CXX_FLAGS="-stdlib=libc++" \
   -DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAGS}" \
   -DCMAKE_SHARED_LINKER_FLAGS="${LINKER_FLAGS}" \
   -DCMAKE_MODULE_LINKER_FLAGS="${LINKER_FLAGS}" \
@@ -61,9 +42,8 @@ CC=clang CXX=clang++ \
   -DHARNESS_PIPELINE_CONFIG_FILE=${PIPELINE_CONFIG_FILE} \
   -DHARNESS_BMK_CONFIG_FILE=${BMK_CONFIG_FILE} \
   -DBMK_CLASS=${BMK_CLASS} \
-  -DLLVMPOLLY_ROOT=${LLVMPOLLY_ROOT} \
+  -DAnnotateLoops_DIR=${AnnotateLoops_DIR} \
   "${SRC_DIR}"
-
 
 exit $?
 
