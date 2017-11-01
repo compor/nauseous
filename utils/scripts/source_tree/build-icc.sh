a17e3875f614d3a17d3bed4963ca4d68b008f745
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-11-01 11:00:19 +0000

    update flags for intel icc version 18.0.0 20170811

---
+++ utils/scripts/source_tree/build-icc.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+# initialize configuration vars
+
+SRC_DIR=""
+INSTALL_PREFIX=""
+
+
+# set configuration vars
+
+if [ -z "$1" ]; then 
+  echo "error: source directory was not provided" 
+
+  exit 1
+fi
+
+SRC_DIR=$1
+
+if [ -z "$2" ]; then 
+  INSTALL_PREFIX="${SRC_DIR}/../install/"
+else
+  INSTALL_PREFIX="$2"
+fi
+
+
+BMK_CONFIG_FILE="${SRC_DIR}/config/suite_all.txt"
+BMK_CLASS="S"
+
+
+# print configuration vars
+
+echo "info: printing configuration vars"
+echo "info: source dir: ${SRC_DIR}"
+echo "info: install dir: ${INSTALL_PREFIX}"
+echo ""
+
+
+#C_FLAGS="-g -Wall -O3"
+#LINKER_FLAGS="-Wl,-L$(llvm-config --libdir) -Wl,-rpath=$(llvm-config --libdir)"
+#LINKER_FLAGS="${LINKER_FLAGS} -lc++ -lc++abi" 
+
+CC=icc CXX=icpc \
+cmake \
+  -DCMAKE_POLICY_DEFAULT_CMP0056=NEW \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=On \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_C_FLAGS="${C_FLAGS}" \
+  -DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_SHARED_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_MODULE_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" \
+  -DHARNESS_BMK_CONFIG_FILE=${BMK_CONFIG_FILE} \
+  -DBMK_CLASS=${BMK_CLASS} \
+  "${SRC_DIR}"
+
+
+exit $?
+
