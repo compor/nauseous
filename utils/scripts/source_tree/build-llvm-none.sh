c91865c99d1125400987f7c5af60d650b0f7ee4f
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-06-20 11:33:38 +0000

    update build script

--- utils/scripts/source_tree/build-llvm-none.sh
+++ utils/scripts/source_tree/build-llvm-none.sh
@@ -14,14 +14,15 @@ IS_BMK_CLASS="C"
 
 C_FLAGS="-g -Wall -O2 -mcmodel=medium"
 #LINKER_FLAGS="-Wl,-L$(llvm-config --libdir) -Wl,-rpath=$(llvm-config --libdir)"
-#LINKER_FLAGS="${LINKER_FLAGS} -lc++ -lc++abi" 
+#LINKER_FLAGS="${LINKER_FLAGS} -lc++ -lc++abi"
 
-CC=clang CXX=clang++ \
 cmake \
   -GNinja \
+  -DCMAKE_C_COMPILER=clang \
+  -DCMAKE_CXX_COMPILER=clang++ \
   -DCMAKE_POLICY_DEFAULT_CMP0056=NEW \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=On \
-  -DLLVM_DIR=$(llvm-config --prefix)/share/llvm/cmake/ \
+  -DLLVM_DIR="$(llvm-config --prefix)/share/llvm/cmake/" \
   -DCMAKE_BUILD_TYPE=Debug \
   -DCMAKE_C_FLAGS="${C_FLAGS}" \
   -DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAGS}" \
@@ -29,7 +30,7 @@ cmake \
   -DCMAKE_MODULE_LINKER_FLAGS="${LINKER_FLAGS}" \
   -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" \
   -DHARNESS_USE_LLVM=On \
-  -DHARNESS_BMK_CONFIG_FILE=${BMK_CONFIG_FILE} \
+  -DHARNESS_BMK_CONFIG_FILE="${BMK_CONFIG_FILE}" \
   -DBMK_CLASS=${BMK_CLASS} \
   -DMG_BMK_CLASS=${MG_BMK_CLASS} \
   -DIS_BMK_CLASS=${IS_BMK_CLASS} \
