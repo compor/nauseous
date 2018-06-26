c7828bb98fd4120022760fc4d1a0b062d76e5cfc
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-06-26 09:36:06 +0000

    detect pgo

--- cmake/Harness-Utils.cmake
+++ cmake/Harness-Utils.cmake
@@ -22,3 +22,51 @@ function(harness_list_prepend)
   set(${HLP_OUTPUT_LIST} ${TMPLIST} PARENT_SCOPE)
 endfunction()
 
+function(harness_detect_pgo)
+  set(options)
+  set(oneValueArgs TYPE MODE PROFILE_FILE)
+  set(multiValueArgs FLAGS)
+
+  cmake_parse_arguments(HDP
+    "${options}"
+    "${oneValueArgs}"
+    "${multiValueArgs}"
+    ${ARGN})
+
+  foreach(LISTITEM ${HDP_FLAGS})
+    if(NOT ${LISTITEM} MATCHES "-fprofile-.*")
+      message(FATAL_ERROR "Not a valid PGO flag.")
+    endif()
+  endforeach()
+
+  list(LENGTH HDP_FLAGS HDP_FLAGS_LEN)
+
+  if(NOT HDP_FLAGS_LEN EQUAL 1)
+    message(FATAL_ERROR "Incorrect number of profiling flags.")
+  endif()
+
+  if("${HDP_FLAGS}" MATCHES "-fprofile-instr.*")
+    set(${HDP_TYPE} "INSTRUMENTATION" PARENT_SCOPE)
+
+    if("${HDP_FLAGS}" MATCHES "-fprofile-instr-gen.*")
+      set(${HDP_MODE} "GENERATE" PARENT_SCOPE)
+    elseif("${HDP_FLAGS}" MATCHES "-fprofile-instr-use.*")
+      set(${HDP_MODE} "USE" PARENT_SCOPE)
+    else()
+      message(FATAL_ERROR "Uknown PGO mode.")
+    endif()
+  elseif("${HDP_FLAGS}" MATCHES "-fprofile-sample.*")
+    set(${HDP_TYPE} "SAMPLE" PARENT_SCOPE)
+
+    if("${HDP_FLAGS}" MATCHES "-fprofile-sample-use.*")
+      set(${HDP_MODE} "USE" PARENT_SCOPE)
+    else()
+      message(FATAL_ERROR "Uknown PGO mode.")
+    endif()
+  else()
+    message(FATAL_ERROR "Uknown PGO type.")
+  endif()
+
+  # TODO profile file needs to parsed
+endfunction()
+
