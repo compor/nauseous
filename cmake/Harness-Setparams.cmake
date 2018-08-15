f48b0f68a10724340b3e38cafdc57c15852edbb8
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-08-15 13:24:20 +0000

    convert custom target to non-dummy

--- cmake/Harness-Setparams.cmake
+++ cmake/Harness-Setparams.cmake
@@ -14,11 +14,15 @@ function(harness_attach_setparams_target)
     ${ARGN})
 
   set(ATTACHED_SP_TARGET ${SP_TARGET}_setparams)
+  set(HEADER_NAME "npbparams.h")
 
-  add_custom_target(${ATTACHED_SP_TARGET}
+  add_custom_command(OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${HEADER_NAME}"
     COMMAND ${SP_UTILITY_EXE} ${SP_TARGET} ${SP_BMK_CLASS}
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-    COMMENT "Generating npbparams.h")
+    COMMENT "Generating ${HEADER_NAME}")
+
+  add_custom_target(${ATTACHED_SP_TARGET}
+    DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/${HEADER_NAME}")
 
   add_dependencies(${SP_TARGET} ${ATTACHED_SP_TARGET})
   add_dependencies(${ATTACHED_SP_TARGET} ${SP_UTILITY_TARGET})
