3a3c367ad0d24be4be6ef706f16e271c94ecc053
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2019-09-23 18:16:09 +0100

    Add build script

---
+++ utils/scripts/source_tree/build-gcc-gprof.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+PRJ_ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../../" && pwd)"
+SRC_DIR=${1:-$PRJ_ROOT_DIR}
+INSTALL_PREFIX=${2:-../install/}
+
+BMK_CONFIG_FILE="${SRC_DIR}/config/suite_all.txt"
+BMK_CLASS="B"
+#DC_BMK_CLASS="A"
+MG_BMK_CLASS="C"
+IS_BMK_CLASS="C"
+
+#
+
+C_FLAGS=""
+C_FLAGS="${C_FLAGS} -Wall"
+C_FLAGS="${C_FLAGS} -g"
+C_FLAGS="${C_FLAGS} -O2"
+C_FLAGS="${C_FLAGS} -mcmodel=medium"
+#C_FLAGS="${C_FLAGS} -fno-unroll-loops"
+#C_FLAGS="${C_FLAGS} -fno-vectorize"
+#C_FLAGS="${C_FLAGS} -fno-slp-vectorize"
+#C_FLAGS="${C_FLAGS} -fsave-optimization-record"
+#C_FLAGS="${C_FLAGS} -fdiagnostics-show-hotness"
+C_FLAGS="${C_FLAGS} -pg"
+
+#LINKER_FLAGS="-Wl,-L$(llvm-config --libdir) -Wl,-rpath=$(llvm-config --libdir)"
+#LINKER_FLAGS="${LINKER_FLAGS} -lc++ -lc++abi"
+
+#
+
+cmake \
+  -GNinja \
+  -DCMAKE_C_COMPILER=gcc \
+  -DCMAKE_CXX_COMPILER=g++ \
+  -DCMAKE_POLICY_DEFAULT_CMP0056=NEW \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=On \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_C_FLAGS="${C_FLAGS}" \
+  -DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_SHARED_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_MODULE_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" \
+  -DHARNESS_BMK_CONFIG_FILE="${BMK_CONFIG_FILE}" \
+  -DBMK_CLASS=${BMK_CLASS} \
+  -DMG_BMK_CLASS=${MG_BMK_CLASS} \
+  -DIS_BMK_CLASS=${IS_BMK_CLASS} \
+  "${SRC_DIR}"
+
+
+  #-DLLVM_DIR=$(llvm-config --prefix)/share/llvm/cmake/ \
+  #-DDC_BMK_CLASS=${DC_BMK_CLASS} \
